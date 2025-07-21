30b151d51820124d5a8fd8160e6dfa4c4491f603
Albacc15 <90273190+Albacc15@users.noreply.github.com>
2025-07-21 14:38:09 +0200

    Update service-worker.js
    with complete routes and questions.json path

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,16 @@ self.addEventListener('install', function(e) {
   e.waitUntil(
     caches.open('az900-store').then(function(cache) {
       return cache.addAll([
-        './index.html',
-        './manifest.json'
+        '/Learn_azure900-pwa/index.html',
+        '/Learn_azure900-pwa/test.html',
+        '/Learn_azure900-pwa/learn.html',
+        '/Learn_azure900-pwa/progress.html',
+        '/Learn_azure900-pwa/test.js',
+        '/Learn_azure900-pwa/learn.js',
+        '/Learn_azure900-pwa/progress.js',
+        '/Learn_azure900-pwa/questions.json',
+        '/Learn_azure900-pwa/manifest.json',
+        '/Learn_azure900-pwa/icon.png',
       ]);
     })
   );
